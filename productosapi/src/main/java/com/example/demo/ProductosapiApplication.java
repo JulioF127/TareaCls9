a8644d3fbbc45c25d93469a9d6dc21a733559bf9
JulioF127 <juliofuentes12@hotmail.com>
2021-04-22 23:56:49 -0600

    Final2

---
+++ productosapi/src/main/java/com/example/demo/ProductosapiApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductosapiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProductosapiApplication.class, args);
+	}
+}
---
+++ productosapi/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+Insert into productos(id, nombre, precio) values (1, 'smartphone',2000);
+Insert into productos (id, nombre, precio) values (2,'audifonos',150);
+Insert into productos (id, nombre, precio) values (3,'television',2500);
